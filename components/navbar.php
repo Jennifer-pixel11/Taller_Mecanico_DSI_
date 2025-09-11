fbf2345cbed5fad5916178fddc5f8b8f97a1242c
IrisArias <ap18018@ues.edu.sv>
2025-09-10 21:46:07 -0600

    otros cambios

--- components/navbar.php
+++ components/navbar.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="icon" href="public/logotipo.png" type="image/png">
   <link rel="stylesheet" type="text/css" href="/Taller_Mecanico_DSI_/static/css/style.css">
   <link rel="stylesheet" type="text/css" href="/Taller_Mecanico_DSI_/public/styles.css">
 </head>
